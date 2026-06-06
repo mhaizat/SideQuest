268a68660017bc3eda67c1600ff1f6191c038921
mhaizat <haizathaneefa.hh@gmail.com>
2026-06-06 12:07:55 +0800

    chore: create custom gamemode

---
+++ Source/SideQuest/CustomGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SideQuestGameMode.h"
+#include "CustomGameMode.generated.h"
+
+UCLASS()
+class SIDEQUEST_API ACustomGameMode : public ASideQuestGameMode
+{
+	GENERATED_BODY()
+	
+};
